fc397a1acd6d3905b317f76edc397c606ea19af1
Nick (ncreel) <ncreel@marlboro.edu>
2020-03-06 13:30:00 -0500

    change images

--- public/js/dollhouse.js
+++ public/js/dollhouse.js
@@ -2,34 +2,34 @@ function handleClick(id){
   const element = document.getElementById(id);
   switch (id) {
     case "kitchen":
-      if(element.src ="public/assets/images/dollhouse/cleankitchen.png"){
-        element.src = "public/assets/images/dollhouse/corruptkitchen.png";
+      if(element.src ="../assets/images/dollhouse/cleankitchen.png"){
+        element.src = "../assets/images/dollhouse/corruptkitchen.png";
       }else{
-        element.src = "public/assets/images/dollhouse/cleankitchen.png";
+        element.src = "../assets/images/dollhouse/cleankitchen.png";
       };
       alert(kitchen);
       break;
     case "bathroom":
-      if(element.src ="public/assets/images/dollhouse/cleanbathroom.png"){
-        element.src = "public/assets/images/dollhouse/corruptbathroom.png";
+      if(element.src ="../assets/images/dollhouse/cleanbathroom.png"){
+        element.src = "../assets/images/dollhouse/corruptbathroom.png";
       }else{
-        element.src = "public/assets/images/dollhouse/cleanbathroom.png";
+        element.src = "../assets/images/dollhouse/cleanbathroom.png";
       };
       alert(bathroom);
       break;
     case "bedroom":
-      if(element.src ="public/assets/images/dollhouse/cleanbedroom.png"){
-        element.src = "public/assets/images/dollhouse/corruptbedroom.png";
+      if(element.src ="../assets/images/dollhouse/cleanbedroom.png"){
+        element.src = "../assets/images/dollhouse/corruptbedroom.png";
       }else{
-        element.src = "public/assets/images/dollhouse/cleanbedroom.png";
+        element.src = "../assets/images/dollhouse/cleanbedroom.png";
       };
       alert(bedroom);
       break;
     case "closet":
-      if(element.src ="public/assets/images/dollhouse/cleancloset.png"){
-        element.src = "public/assets/images/dollhouse/corruptcloset.png";
+      if(element.src ="../assets/images/dollhouse/cleancloset.png"){
+        element.src = "../assets/images/dollhouse/corruptcloset.png";
       }else{
-        element.src = "public/assets/images/dollhouse/cleancloset.png";
+        element.src = "../assets/images/dollhouse/cleancloset.png";
       };
       alert(closet);
     }
