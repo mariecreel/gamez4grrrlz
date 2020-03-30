331a9d26879d6721aaabe284125b196ef7a9864a
Nick (ncreel) <ncreel@marlboro.edu>
2020-03-30 12:47:15 -0400

    remove shapes from index.html,

--- public/js/cans.js
+++ public/js/cans.js
@@ -1,3 +1,26 @@
+const peas = "<img class='canimg' src='../assets/images/cans/sweetpeas.png'>";
+const beans = "<img class='canimg' src='../assets/images/cans/greenbeans.png'>";
+const potato = "<img class='canimg' src='../assets/images/cans/sweetpotatoes.png'>";
+const peaches = "<img class='canimg' src='../assets/images/cans/peaches.png'>";
+
+const canimgs = [peas, beans, potato, peaches]
+
+function randomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function handleClick(id){
-  document.getElementById(id).innerHTML = stanzas[id];
+  if(id != "nine"){
+    document.getElementById(id).innerHTML = stanzas[id];
+    if(document.getElementById("nine").innerHTML.charAt(1) === 'p'){
+      document.getElementById("nine").innerHTML = canimgs[randomInt(4)]
+    }
+  } else {
+    document.getElementById(id).innerHTML = stanzas[id];
+    for(key in stanzas){
+      if(key != "nine"){
+        document.getElementById(key).innerHTML = canimgs[randomInt(4)]
+      }
+    }
+  }
 }
