30433ff7bf288133b37c99776f782e239f477ad6
Nick (ncreel) <ncreel@marlboro.edu>
2020-04-12 13:58:11 -0400

    remove line breaks from control

--- public/js/cansstanzas.js
+++ public/js/cansstanzas.js
@@ -1,71 +1,71 @@
 const zero = '<p> \
-        The doctor diagnosed me with <br/> \
-        obesity when I was nine<br/> \
-        and told me not to eat past six<br/> \
-        o’ clock, unless I ate fresh fruit.<br/> \
+        The doctor diagnosed me with\
+        obesity when I was nine \
+        and told me not to eat past six \
+        o’ clock, unless I ate fresh fruit. \
       </p>';
 
 const one = '<p>\
-        On occasion, oranges and<br/>\
-        apples rolled around our fridge shelves<br/>\
-        but often fruit came from tin cans— <br/>\
-        I liked to drink the syrup, too. <br/>\
+        On occasion, oranges and\
+        apples rolled around our fridge shelves\
+        but often fruit came from tin cans— \
+        I liked to drink the syrup, too. \
       </p>';
 
 const two = '<p>\
-        The cabinet, always stuffed with cans,<br/>\
-        was my first stop after the bus<br/>\
-        ride, often for Chef Boyardee,<br/>\
-        cling peaches, or instant noodles. <br/>\
+        The cabinet, always stuffed with cans,\
+        was my first stop after the bus\
+        ride, often for Chef Boyardee,\
+        cling peaches, or instant noodles. \
       </p>';
 
 const three = '<p>\
-        Mom made dinner past six o’clock:<br/>\
-        bolognese, stroganoff, sloppy<br/>\
-        joes, hamburger steak and gravy,<br/>\
-        and cheeseburger macaroni,<br/>\
+        Mom made dinner past six o’clock:\
+        bolognese, stroganoff, sloppy\
+        joes, hamburger steak and gravy,\
+        and cheeseburger macaroni,\
       </p>';
 
 const four = '<p>\
-        always with a tin of sweet peas<br/>\
-        or french cut green beans, microwaved<br/>\
-        with margarine, salt, and pepper—<br/>\
-        She let me choose the vegetable.<br/>\
+        always with a tin of sweet peas\
+        or french cut green beans, microwaved\
+        with margarine, salt, and pepper—\
+        She let me choose the vegetable.\
       </p>';
 
 const five = '<p>\
-        We never bought cans at the store.<br/>\
-        An abundance I overlooked<br/>\
-        was stacked in our kitchen cabinet<br/>\
-        from the bottom to the counter.<br/>\
+        We never bought cans at the store.\
+        An abundance I overlooked\
+        was stacked in our kitchen cabinet\
+        from the bottom to the counter.\
       </p>';
 
 const six = '<p>\
-        In each classroom, the teacher put<br/>\
-        a box covered in construction<br/>\
-        paper. Big black bubble letters<br/>\
-        read “NON-PERISHABLE FOOD DRIVE.”<br/>\
+        In each classroom, the teacher put\
+        a box covered in construction\
+        paper. Big black bubble letters\
+        read “NON-PERISHABLE FOOD DRIVE.”\
       </p>';
 
 const seven = '<p>\
-        As the box was slowly filled with<br/>\
-        donations from my classmates, I<br/>\
-        felt exposed, walking through the door<br/>\
-        with no donation of my own.<br/>\
+        As the box was slowly filled with\
+        donations from my classmates, I\
+        felt exposed, walking through the door\
+        with no donation of my own.\
       </p>';
 
 const eight = '<p>\
-        That evening, I packed a plastic<br/>\
-        grocery bag with cans of peas,<br/>\
-        yams, green beans, ramen noodles, and<br/>\
-        even a can of Boyardee.<br/>\
+        That evening, I packed a plastic\
+        grocery bag with cans of peas,\
+        yams, green beans, ramen noodles, and\
+        even a can of Boyardee.\
       </p>';
 
 const nine = '<p>\
-        I brought cans to school for the drive, <br/>\
-        and I felt I’d done a good deed.<br/>\
-        When mom came home from church that week,<br/>\
-        she brought home a box filled with cans. <br/>\
+        I brought cans to school for the drive, \
+        and I felt I’d done a good deed.\
+        When mom came home from church that week,\
+        she brought home a box filled with cans. \
       </p>';
 
 const stanzas = {
